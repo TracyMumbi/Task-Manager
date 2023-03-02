893f5b362b8bd8f30c846f2ec5f934221952c13c
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2023-03-02 15:19:37 +0300

    created a form to hold my data

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -2,8 +2,29 @@ import React from 'react'
 
 function NewTask() {
   return (
-    <div>NewTask</div>
+    <div>
+        <form>
+            <label>Name</label>
+            <input type="text" placeholder='Name' /><br />
+            <label>Description</label>
+            <input type="text" placeholder='Description' /><br />
+            <label>Status</label>
+            <input type="text" placeholder='Status' /><br />
+            <label>Date</label>
+            <input type="text" placeholder='Date' /><br />
+            <label>Due Date</label>
+            <input type="text" placeholder='Due Date' /><br />
+        </form>
+    </div>
   )
 }
 
 export default NewTask
+
+
+// t.string :name
+// t.string :description
+// t.string :completion_status
+// t.date :date
+// t.date :due_date
+// t.integer :user_id
